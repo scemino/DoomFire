b2d6e6feb13cd318f55aeb926b69a4d96e079dcb
scemino <scemino74@gmail.com>
2020-08-29 20:08:58 +0200

    Fix main.cpp compilation with Windows
    Fixes #1

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "DoomFireApplication.h"
 #include <iostream>
 
-int main(int argc, const char **argv) {
+int main(int argc, const char *argv[]) {
   DoomFireApplication app;
   app.run();
   return EXIT_SUCCESS;
